83c96ebbbec6f244d76fd7267e10c40a1ae27116
Soumya <soumya.pullakhandam1@gmail.com>
2020-05-23 19:39:32 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>updated</p>
+        <p>I was changed Again</p>
       </header>
     </div>
   );
